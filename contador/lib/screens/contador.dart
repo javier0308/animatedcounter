f5e69ca937e08099ef94d4eecfcfd818b32a15cd
unknown <javierchoquetico@gmail.com>
2022-12-20 20:28:35 -0500

    Mi primer commit

---
+++ contador/lib/screens/contador.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+  int? _oldcounter;
+
+  void increment() {
+    _counter++;
+    setState(() {});
+  }
+
+  void decrecer() {
+    if (_counter > 0) {
+      _counter--;
+    }
+    setState(() {});
+  }
+
+  void reset() {
+    _counter = 0;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador animado'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Numero de clicks',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TweenAnimationBuilder<double>(
+              tween: Tween(begin: 0.0, end: 1.0),
+              key: Key(_counter.toString()),
+              duration: const Duration(milliseconds: 700),
+              builder: (context, value, _) {
+                return Stack(
+                  children: [
+                    if (_oldcounter != null)
+                      //Texto de old value
+                      Opacity(
+                        opacity: 1 - value,
+                        child: Transform.translate(
+                          offset: Offset(50 * value, 0.0),
+                          child: Text(
+                            _oldcounter.toString(),
+                            style: const TextStyle(fontSize: 80),
+                          ),
+                        ),
+                      ),
+                    Opacity(
+                      opacity: value,
+                      child: Transform.translate(
+                        offset: Offset(-50 * (1 - value), 0.0),
+                        child: Text(
+                          _counter.toString(),
+                          style: const TextStyle(fontSize: 80),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingButton(
+        decrement: decrecer,
+        increment: increment,
+        reset: reset,
+      ),
+    );
+  }
+}
+
+class CustomFloatingButton extends StatelessWidget {
+  final Function increment, decrement, reset;
+  const CustomFloatingButton({
+    Key? key,
+    required this.increment,
+    required this.decrement,
+    required this.reset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        FloatingActionButton(
+            backgroundColor: Colors.deepPurple,
+            child: const Icon(
+              Icons.exposure_plus_1_outlined,
+            ),
+            onPressed: () => increment()),
+        FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          child: const Icon(
+            Icons.restart_alt,
+          ),
+          onPressed: () => reset(),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          child: const Icon(
+            Icons.exposure_minus_1_outlined,
+          ),
+          onPressed: () => decrement(),
+        ),
+      ],
+    );
+  }
+}
